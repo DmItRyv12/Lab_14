1638e957e1bbbe9c31efb5cfe55a32f52e642c8e
Дмитрий Минаков <m1nak0vd@yandex.ru>
2022-01-16 20:44:20 +0300

    Добавьте файлы проекта.

---
+++ Lab14/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    abstract class Figure
+    {
+        public abstract double GetArea();
+        public abstract double GetPerimetr();
+        public abstract string Neme{get; set;}
+
+    }
+
+    class Rectangular : Figure
+    {
+        string name;
+
+        public double Width { get; set; } 
+        
+        public double Height { get; set; }
+
+        public override double GetArea()
+        {
+
+            return Width * Height ;
+        }
+
+        public override double GetPerimetr()
+        {
+            return Width * 2 + Height * 2;
+        }
+        public override string Neme
+        {
+            get
+            {
+              return name;
+            }
+            set
+            {
+              name = value;
+            }
+        
+        
+        }
+    }
+    
+}
---
+++ Lab_14/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Triangle triangle = new Triangle(1, 2, 3);
+            // RectangularTriangle rectangularTriangle = new RectangularTriangle(4,5);
+            RegularTriangle regularTriangle = new RegularTriangle();
+            Triangle triangle = regularTriangle;
+            if (triangle is RectangularTriangle)
+            {
+                // RectangularTriangle rectangularTriangle1 = triangle as RectangularTriangle;
+                RectangularTriangle rectangularTriangle1 = (RectangularTriangle)triangle;
+            }
+
+            // Triangle triangle = rectangularTriangle;
+            // RectangularTriangle rectangularTriangle1 = (RectangularTriangle)triangle;
+            Console.ReadKey();
+        }
+    }
+    class Triangle
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+        public Triangle()
+        {
+
+        }
+        public Triangle(double a, double b, double c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+
+        //public virtual double GetArea()
+        public double GetArea()
+        {
+            double p = (A + B + C) / 2;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+    }
+    class RectangularTriangle : Triangle
+    {
+        public RectangularTriangle(double a, double b)
+        //      : base(a, b, Math.Sqrt(a * a + b * b))
+        {
+            A = a;
+            B = b;
+            C = Math.Sqrt(a * a + b * b);
+        }
+
+        //public override double GetArea()
+        public new double GetArea()
+        {
+        return A*B/2;  
+        }
+    }
+
+    class RegularTriangle : Triangle
+    {
+
+    }
+}
